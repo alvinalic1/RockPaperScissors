0a2b68114ebe91e28710879bfef22dfbd77d2c71
Alvin Alic <alvin.alic@gmail.com>
2024-04-18 18:40:10 -0500

    Add comments for each function so I can come back later and know what I did and why

--- javascript.js
+++ javascript.js
@@ -1,19 +1,30 @@
 console.log("Hello");
 
+//array holding possible choices
 const RPS = ["Rock", "Paper", "Scissors"];
 
+/*
+Method that will get a random number and use that number to access one of the choices in the RPS Array
+*/
 function getComputerChoice()
 {
     let number = Math.floor(Math.random()*RPS.length)
     return RPS[number];
 }
 
+/*
+A prompt will pop up to get user input
+*/
 function getPlayerChoice()
 {
     let choice = prompt(RPS);
     return choice;
 }
 
+/*
+Compares the computer choice with the player choice
+returns winner
+*/
 function playRound(getComputerChoice, getPlayerChoice)
 {
     let player = getPlayerChoice.toLowerCase();
@@ -33,7 +44,10 @@ function playRound(getComputerChoice, getPlayerChoice)
     }
 }
 
-
+/*
+Final game
+Asks user how many rounds they wish to play and goes through that many times
+*/
 function finalGame()
 {
     let numberOfRound = prompt("How many round would you like to play?")
